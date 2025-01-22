f39a390fd6ea1423589ecee4f6c5b4cd2807c62c
kotovnik <kotovnik2024tbilisi@gmail.com>
2025-01-22 20:33:08 +0400

    fix: delete comments

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const myPath = __dirname;
-// console.log(__dirname);
-// console.log(__filename);
 const pathInfo = {
     fileName: path.basename(myPath),
     folderName: path.dirname(myPath),
@@ -12,8 +10,6 @@ const pathInfo = {
 }
 const file = 'text.txt'
 const pathToText = path.join(pathInfo.folderName, pathInfo.fileName, file);
-// const pathToText = [pathInfo.folderName, pathInfo.fileName,].join('/');
-// console.log(pathToText);
 const read = fs.createReadStream(pathToText, 'utf8');
 read.on('data', (text) => {
     console.log(text);
