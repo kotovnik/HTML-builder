2622a14f1a871b51a0cc37d9969d6f07bcbc9035
kotovnik <kotovnik2024tbilisi@gmail.com>
2025-01-22 22:59:32 +0400

    feat: add solve second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const readLine = require('readline');
+const myPath = __dirname;
+const pathInfo = {
+    fileName: path.basename(myPath),
+    folderName: path.dirname(myPath),
+    fileExtension: path.extname(myPath),
+    absoluteOrNot: path.isAbsolute(myPath),
+    detailInfo: path.parse(myPath),
+}
+const file = 'text.txt'
+const pathToText = path.join(pathInfo.folderName, pathInfo.fileName, file);
+const writeStream = fs.createWriteStream(pathToText, { flags: 'a' });
+const read = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+console.log("Enter the text to write it to a file. To exit, enter 'q'");
+function writeText() {
+    read.question("Enter the text (or something): ", (text) =>{
+        if((text === 'q') || (text === 'й')){
+            console.log("You're out of the input");
+            read.close();
+            writeStream.end();
+        } else {
+            writeStream.write(text);
+            writeText();
+        }
+    });
+};
+writeText();
