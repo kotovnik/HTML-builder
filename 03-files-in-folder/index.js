6a1daf7869731b4223006c7214b5894b189b73b6
kotovnik <kotovnik2024tbilisi@gmail.com>
2025-01-23 04:04:22 +0400

    refactor: delete comments from third task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const myPath = __dirname;
-console.log(myPath);
 const folder = 'secret-folder';
 const pathToFolder = path.join(myPath, folder);
-console.log(pathToFolder);
-// fs.readdir(путь, параметры, обратный вызов)
-// <file name>-<file extension>-<file size>
 async function getInfoOfFiles() {
     try {
         const allFiles = await fs.promises.readdir(pathToFolder, { withFileTypes: true });
         for (const file of allFiles) {
             const fileName = file.name;
-            // console.log(`fileName: ${fileName}`);
+            console.log(fileName);
             const pathToFile = path.join(pathToFolder, fileName);
             if (file.isFile()) {
                 const fileExtension = path.extname(fileName);
